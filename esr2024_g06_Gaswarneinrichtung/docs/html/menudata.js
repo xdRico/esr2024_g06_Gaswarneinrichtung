c5933568fa36fac964714af7f32ced472e64c545
Fabian <Fabian@Fabian-Laptop>
2024-07-03 23:55:06 +0200

    update doxygen

--- esr2024_g06_Gaswarneinrichtung/docs/html/menudata.js
+++ esr2024_g06_Gaswarneinrichtung/docs/html/menudata.js
@@ -23,24 +23,23 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Related Pages",url:"pages.html"},
-{text:"Data Structures",url:"annotated.html",children:[
-{text:"Data Structures",url:"annotated.html"},
-{text:"Data Structure Index",url:"classes.html"}]},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"},
-{text:"Globals",url:"globals.html",children:[
-{text:"All",url:"globals.html",children:[
+{text:"Hauptseite",url:"index.html"},
+{text:"Dateien",url:"files.html",children:[
+{text:"Auflistung der Dateien",url:"files.html"},
+{text:"Globale Elemente",url:"globals.html",children:[
+{text:"Alle",url:"globals.html",children:[
 {text:"a",url:"globals.html#index_a"},
+{text:"c",url:"globals.html#index_c"},
 {text:"g",url:"globals.html#index_g"},
 {text:"i",url:"globals.html#index_i"},
 {text:"l",url:"globals.html#index_l"},
 {text:"m",url:"globals.html#index_m"},
+{text:"o",url:"globals.html#index_o"},
 {text:"p",url:"globals.html#index_p"},
 {text:"s",url:"globals.html#index_s"},
-{text:"t",url:"globals.html#index_t"}]},
-{text:"Functions",url:"globals_func.html",children:[
+{text:"t",url:"globals.html#index_t"},
+{text:"w",url:"globals.html#index_w"}]},
+{text:"Funktionen",url:"globals_func.html",children:[
 {text:"a",url:"globals_func.html#index_a"},
 {text:"g",url:"globals_func.html#index_g"},
 {text:"i",url:"globals_func.html#index_i"},
@@ -49,4 +48,5 @@ var menudata={children:[
 {text:"p",url:"globals_func.html#index_p"},
 {text:"s",url:"globals_func.html#index_s"},
 {text:"t",url:"globals_func.html#index_t"}]},
-{text:"Macros",url:"globals_defs.html"}]}]}]}
+{text:"Variablen",url:"globals_vars.html"},
+{text:"Makrodefinitionen",url:"globals_defs.html"}]}]}]}
