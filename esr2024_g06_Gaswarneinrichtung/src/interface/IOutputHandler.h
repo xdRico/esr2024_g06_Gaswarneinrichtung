ad4e539bf0fed7e5f4759567b96e40556bc119e8
Fabian <Fabian@134.103.204.14>
2024-07-03 12:17:45 +0200

    Update Ioutputhandler.h

--- esr2024_g06_Gaswarneinrichtung/src/interface/IOutputHandler.h
+++ esr2024_g06_Gaswarneinrichtung/src/interface/IOutputHandler.h
@@ -48,48 +48,5 @@ void setAlertLED(bool alert);
 void setOnLED(bool on);
 
 
-///**
-// * getOnState():
-// *
-// * This method is used to get the state, if the device is switched on
-// *
-// *  return:
-// *      bool: if the device is switched on
-// */
-//bool getOnState();
-//
-//
-///**
-// * setOnState(bool isOn):
-// *
-// * This method is used to set the state, if the device is switched on
-// *
-// *  param:
-// *      isOn: the bool that defines, if the device is switched on
-// */
-//void setOnState(bool isOn);
-//
-//
-///**
-// * setWarningState(bool warningState):
-// *
-// * This method is used to set the state, if the device is warning for gas
-// *
-// *  param:
-// *      warningState: the bool that defines, if the device is in warning-mode
-// */
-//void setWarningState(bool warningState);
-//
-//
-///**
-// * setAlertState(bool alertState):
-// *
-// * This method is used to set the state, if the device is alarming for non-movement
-// *
-// *  param:
-// *      alertState: the bool that defines, if the device is in alert-mode
-// */
-//void setAlertState(bool alertState);
-
 
 #endif /* SRC_INTERFACE_IOUTPUTHANDLER_H_ */
